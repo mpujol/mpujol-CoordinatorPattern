a7d41ab77e922ff3b2f972d0f130693f02120ffe
Mike Pujol <mike.pujol@redfin.com>
2020-04-16 17:09:34 -0700

    Initial Commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Mike Pujol on 4/16/20.
+//  Copyright © 2020 Mike Pujol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
